e11c4e20bddd684e0745f5bafcfd29c7862b81dc
phan.dang.hai.vu <SUN-ASTERISK\phan.dang.hai.vu@b121968-pc.sun-asterisk.com>
2020-09-17 18:07:41 +0700

    fix comment - split conditions to method

--- app/services/exercise6/calculate_free_parking_time_service.rb
+++ app/services/exercise6/calculate_free_parking_time_service.rb
@@ -10,31 +10,44 @@ module Exercise6
     end
 
     def perform
-      @amount = validate_amount(@amount)
-      @total_free_parking_time += @settings.extra_free_parking_time_for_movie if @watch_movie == 'true'
+      @amount = validate_amount @amount
+      @total_free_parking_time += settings.extra_free_parking_time_for_movie if watched_movie?
 
-      if @amount >= @settings.discount_level_1 && @amount < @settings.discount_level_2
-        @total_free_parking_time += @settings.level_1_free_parking_time
-      elsif @amount >= @settings.discount_level_2
-        @total_free_parking_time += @settings.level_2_free_parking_time
+      if amount_match_first_level_discount?
+        @total_free_parking_time += settings.level_1_free_parking_time
+      elsif amount_match_second_level_discount?
+        @total_free_parking_time += settings.level_2_free_parking_time
       end
 
-      response
+      response true
     rescue ArgumentError
-      response
+      response false
     end
 
     private
+    attr_reader :settings, :validations, :watch_movie
 
-    def validate_amount(number)
-      return number.to_i if number.to_s.match?(@validations.number)
+    def watched_movie?
+      watch_movie == 'true'
+    end
+
+    def amount_match_first_level_discount?
+      @amount >= settings.discount_level_1 && @amount < settings.discount_level_2
+    end
+
+    def amount_match_second_level_discount?
+      @amount >= settings.discount_level_2
+    end
+
+    def validate_amount number
+      return number.to_i if number.to_s.match?(validations.number)
 
       @errors.merge!(amount: :invalid)
       raise ArgumentError
     end
 
-    def response
-      OpenStruct.new(total_free_parking_time: @total_free_parking_time, errors: @errors)
+    def response(result)
+      OpenStruct.new success?: result, total_free_parking_time: @total_free_parking_time, errors: @errors
     end
   end
 end
