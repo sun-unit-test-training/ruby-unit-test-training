e1981c6da48648d4f2f9644cb4a22805d41e208e
dobt-1508 <bui.tien.do@sun-asterisk.com>
2020-09-18 14:59:54 +0700

    add errors to input

--- app/controllers/exercise4_controller.rb
+++ app/controllers/exercise4_controller.rb
@@ -1,8 +1,7 @@
 class Exercise4Controller < ApplicationController
   def index
-    @choose_day = {}
-    return if params['day_in_month'].blank?
-
-    @choose_day = Exercise4::CalendarService.new(params[:day_in_month]).perform
+    @response = Exercise4::CalendarService.new(params[:day_in_month]).perform
+    @choose_day = @response.data
+    @errors = @response.errors
   end
 end
--- app/services/exercise4/calendar_service.rb
+++ app/services/exercise4/calendar_service.rb
@@ -4,17 +4,23 @@ module Exercise4
     TYPE_COLOR = %w[red blue black].freeze
 
     def initialize(day_in_month)
-      @day_in_month = day_in_month
+      @day_in_month = day_in_month || ''
+      @settings = Settings.exercise_4
+      @validations = Settings.validations
+      @errors = {}
+      @choose_day = {}
     end
 
     def perform
-      calendar_color
+      response
     end
 
     private
 
+    attr_reader :day_in_month, :validations, :settings
+
     def calendar_color
-      if sun_holiday? || sat_holiday?
+      if holiday? || sunday? || (holiday? && saturday?)
         red_color
       elsif saturday?
         blue_color
@@ -24,26 +30,38 @@ module Exercise4
     end
 
     def param_todate
-      @param_todate ||= @day_in_month.to_date
+      @param_todate ||= day_in_month.to_date
     end
 
-    def sun_holiday?
-      (HOLIDAY.include? param_todate.strftime(Settings.exercise_4.date_month_format)) ||
-        param_todate.sunday?
+    def holiday?
+      (HOLIDAY.include? param_todate.strftime(settings.date_month_format))
     end
 
     def saturday?
       param_todate.saturday?
     end
 
-    def sat_holiday?
-      saturday? && (HOLIDAY.include? param_todate.strftime(Settings.exercise_4.date_month_format))
+    def sunday?
+      param_todate.sunday?
+    end
+
+    def validate_day?
+      is_type_date = day_in_month.match?(validations.date)
+      return true if day_in_month.present? && is_type_date
+
+      @errors.merge!(day_in_month: :invalid) unless day_in_month.blank?
+      false
     end
 
     TYPE_COLOR.each do |value|
       define_method "#{value}_color" do
         { param_todate.day => value }
       end
+    end
+
+    def response
+      @choose_day = calendar_color if validate_day?
+      OpenStruct.new success: validate_day?, data: @choose_day, errors: @errors
     end
   end
 end
