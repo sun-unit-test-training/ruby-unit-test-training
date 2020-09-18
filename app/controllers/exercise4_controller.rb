011f7e0a5c930b7f15c8bc4ac6f51b24f95e7785
dobt-1508 <bui.tien.do@sun-asterisk.com>
2020-09-18 09:01:24 +0700

    fix comment result return

--- app/controllers/exercise4_controller.rb
+++ app/controllers/exercise4_controller.rb
@@ -3,6 +3,6 @@ class Exercise4Controller < ApplicationController
     @choose_day = {}
     return if params['day_in_month'].blank?
 
-    @choose_day = Exercise4::CalendarService.new(params[:day_in_month]).perform || {}
+    @choose_day = Exercise4::CalendarService.new(params[:day_in_month]).perform
   end
 end
