009f0a0233b13e9f9685fbec27c3c837805f0a53
oLuongVanSang <luong.van.sang@sun-asterisk.com>
2020-09-18 09:38:21 +0700

    Refactor code

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,14 +5,18 @@ class Transaction < ApplicationRecord
   }, allow_nil: true
 
   def fee
-    return 0 if is_vip_account?
+    return 0 if is_vip_account? || free_withdraw?
 
-    return 110 if is_holiday? || withdrew_at.on_weekend?
+    110
+  end
+
+  private
+
+  def free_withdraw?
+    return false if is_holiday? || withdrew_at.on_weekend?
 
     free_withdraw_start = withdrew_at.change(hour: 8, min: 45)
     free_withdraw_end = withdrew_at.change(hour: 17, min: 59)
-    return 0 if withdrew_at.between?(free_withdraw_start, free_withdraw_end)
-
-    110
+    withdrew_at.between?(free_withdraw_start, free_withdraw_end)
   end
 end
