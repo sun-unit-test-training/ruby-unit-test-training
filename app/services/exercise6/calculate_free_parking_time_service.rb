24a6d4e9de30fa29f83a490b6bf6a5b8d150a523
Phan Dang Hai Vu <48303281+vupdh-0867@users.noreply.github.com>
2020-09-17 21:39:52 -0400

    Ex 6 - Coding (#29)
    * Ex 6 - Coding
    
    * fix comment - split conditions to method
    
    Co-authored-by: phan.dang.hai.vu <SUN-ASTERISK\phan.dang.hai.vu@b121968-pc.sun-asterisk.com>

---
+++ app/services/exercise6/calculate_free_parking_time_service.rb
@@ -0,0 +1,53 @@
+module Exercise6
+  class CalculateFreeParkingTimeService
+    def initialize(amount, watch_movie)
+      @settings = Settings.exercise_6
+      @validations = Settings.validations
+      @amount = amount
+      @watch_movie = watch_movie
+      @total_free_parking_time = 0
+      @errors = {}
+    end
+
+    def perform
+      @amount = validate_amount @amount
+      @total_free_parking_time += settings.extra_free_parking_time_for_movie if watched_movie?
+
+      if amount_match_first_level_discount?
+        @total_free_parking_time += settings.level_1_free_parking_time
+      elsif amount_match_second_level_discount?
+        @total_free_parking_time += settings.level_2_free_parking_time
+      end
+
+      response true
+    rescue ArgumentError
+      response false
+    end
+
+    private
+    attr_reader :settings, :validations, :watch_movie
+
+    def watched_movie?
+      watch_movie == 'true'
+    end
+
+    def amount_match_first_level_discount?
+      @amount >= settings.discount_level_1 && @amount < settings.discount_level_2
+    end
+
+    def amount_match_second_level_discount?
+      @amount >= settings.discount_level_2
+    end
+
+    def validate_amount number
+      return number.to_i if number.to_s.match?(validations.number)
+
+      @errors.merge!(amount: :invalid)
+      raise ArgumentError
+    end
+
+    def response(result)
+      OpenStruct.new success?: result, total_free_parking_time: @total_free_parking_time, errors: @errors
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   resources :exercise1, only: :index
   resources :exercise9, only: :index
+
+  get "/exercise6", to: "exercise6#free_parking_time"
+  post "/exercise6", to: "exercise6#calculate_free_parking_time"
 end
