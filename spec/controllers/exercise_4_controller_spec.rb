b56da71ac8385cf02367dd67dd0eddc5e7f62bbd
Your Name <buitiendo@sun-asterisk.com>
2020-09-20 08:45:07 +0700

    Rename the variable

--- spec/controllers/exercise_4_controller_spec.rb
+++ spec/controllers/exercise_4_controller_spec.rb
@@ -6,44 +6,44 @@ RSpec.describe Exercise4Controller, type: :controller do
 
     shared_examples 'calendar color' do
       let(:params) {{ day_in_month: day_in_month }}
-      it { expect(assigns(:choose_day)).to eq calerdar_color }
+      it { expect(assigns(:choose_day)).to eq calendar_color }
     end
 
     context 'when day_in_month is holiday' do
       context 'and day_in_month is sunday' do
         let(:day_in_month) { DateTime.new(2020,9,27).strftime('%Y-%m-%d') }
-        let(:calerdar_color) {{ 27=>"red" }}
+        let(:calendar_color) {{ 27=>"red" }}
         include_examples 'calendar color'
       end
 
       context 'and day_in_month is saturday' do
         let(:day_in_month) { DateTime.new(2020,9,05).strftime('%Y-%m-%d') }
-        let(:calerdar_color) {{ 5=>"red" }}
+        let(:calendar_color) {{ 5=>"red" }}
         include_examples 'calendar color'
       end
 
       context 'when day_in_month is normal day' do
         let(:day_in_month) { DateTime.new(2020,9,02).strftime('%Y-%m-%d') }
-        let(:calerdar_color) {{ 2=>"red" }}
+        let(:calendar_color) {{ 2=>"red" }}
         include_examples 'calendar color'
       end
     end
 
     context 'when day_in_month is satuday' do
       let(:day_in_month) { DateTime.new(2020,9,12).strftime('%Y-%m-%d') }
-      let(:calerdar_color) {{ 12=>"blue" }}
+      let(:calendar_color) {{ 12=>"blue" }}
       include_examples 'calendar color'
     end
 
     context 'when day_in_month is sunday' do
       let(:day_in_month) { DateTime.new(2020,9,20).strftime('%Y-%m-%d') }
-      let(:calerdar_color) {{ 20=>"red" }}
+      let(:calendar_color) {{ 20=>"red" }}
       include_examples 'calendar color'
     end
 
     context 'when day_in_month is normal day' do
       let(:day_in_month) { DateTime.new(2020,9,10).strftime('%Y-%m-%d') }
-      let(:calerdar_color) {{ 10=>"black" }}
+      let(:calendar_color) {{ 10=>"black" }}
       include_examples 'calendar color'
     end
 
