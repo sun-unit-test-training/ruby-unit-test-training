614a1e497123bfe08826c26bb49b36709ef67d82
dobt-1508 <bui.tien.do@sun-asterisk.com>
2020-09-21 08:41:05 +0700

    fix param rspec

--- spec/controllers/exercise_4_controller_spec.rb
+++ spec/controllers/exercise_4_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Exercise4Controller, type: :controller do
 
     shared_examples 'calendar color' do
       let(:params) {{ day_in_month: day_in_month }}
+
       it { expect(assigns(:choose_day)).to eq calendar_color }
     end
 
@@ -13,18 +14,21 @@ RSpec.describe Exercise4Controller, type: :controller do
       context 'and day_in_month is sunday' do
         let(:day_in_month) { DateTime.new(2020,9,27).strftime('%Y-%m-%d') }
         let(:calendar_color) {{ 27=>"red" }}
+
         include_examples 'calendar color'
       end
 
       context 'and day_in_month is saturday' do
         let(:day_in_month) { DateTime.new(2020,9,05).strftime('%Y-%m-%d') }
         let(:calendar_color) {{ 5=>"red" }}
+
         include_examples 'calendar color'
       end
 
       context 'when day_in_month is normal day' do
         let(:day_in_month) { DateTime.new(2020,9,02).strftime('%Y-%m-%d') }
         let(:calendar_color) {{ 2=>"red" }}
+
         include_examples 'calendar color'
       end
     end
@@ -32,28 +36,33 @@ RSpec.describe Exercise4Controller, type: :controller do
     context 'when day_in_month is satuday' do
       let(:day_in_month) { DateTime.new(2020,9,12).strftime('%Y-%m-%d') }
       let(:calendar_color) {{ 12=>"blue" }}
+
       include_examples 'calendar color'
     end
 
     context 'when day_in_month is sunday' do
       let(:day_in_month) { DateTime.new(2020,9,20).strftime('%Y-%m-%d') }
       let(:calendar_color) {{ 20=>"red" }}
+
       include_examples 'calendar color'
     end
 
     context 'when day_in_month is normal day' do
       let(:day_in_month) { DateTime.new(2020,9,10).strftime('%Y-%m-%d') }
       let(:calendar_color) {{ 10=>"black" }}
+
       include_examples 'calendar color'
     end
 
     context 'when day_in_month is incorrect format' do
       let(:params) {{ day_in_month: "2020-12nnn-22" }}
+
       it { expect(assigns(:errors)).to eq ({:day_in_month=>:invalid}) }
     end
 
     context 'when params is nil' do
-      let(:params) {{}}
+      let(:params) { nil }
+
       it { expect(assigns(:errors)).to eq ({}) }
     end
   end
