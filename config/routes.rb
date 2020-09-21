319d52c361f628a739e7ef2453ae87b673ddb2f6
Vo Van Quang <49388043+quangvv-1620@users.noreply.github.com>
2020-09-21 08:57:23 +0700

    Ex10 - Implement coding (#35)
    * Ex9: Implement coding
    
    * fix cmt
    
    Co-authored-by: vovanquang12cntt <vanquang451@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   resources :exercise1, only: :index
   resources :exercise2, only: [:new, :create]
+  resources :exercise3, only: :index
   resources :exercise4, only: :index
   resources :exercise5, only: :index
   resources :exercise9, only: :index
 
   get "/exercise6", to: "exercise6#free_parking_time"
   post "/exercise6", to: "exercise6#calculate_free_parking_time"
-  resources :exercise3, only: :index
+  get "exercise10/checkout" => "exercise10#checkout"
+  post "exercise10/checkout" => "exercise10#checkout"
 end
