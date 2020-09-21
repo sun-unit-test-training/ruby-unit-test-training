7e20a5cda1cacee9097db81a5ce2fd30c7d0f786
dobt-1508 <bui.tien.do@sun-asterisk.com>
2020-09-21 11:22:04 +0700

    fix cmt

--- spec/controllers/exercise_4_controller_spec.rb
+++ spec/controllers/exercise_4_controller_spec.rb
@@ -7,63 +7,77 @@ RSpec.describe Exercise4Controller, type: :controller do
     shared_examples 'calendar color' do
       let(:params) {{ day_in_month: day_in_month }}
 
-      it { expect(assigns(:choose_day)).to eq calendar_color }
+      it do
+        expect(assigns(:choose_day)).to eq calendar_color
+        expect(assigns(:errors)).to eq error
+      end
     end
 
     context 'when day_in_month is holiday' do
       context 'and day_in_month is sunday' do
-        let(:day_in_month) { DateTime.new(2020,9,27).strftime('%Y-%m-%d') }
+        let(:day_in_month) { "2020-9-27" }
         let(:calendar_color) {{ 27=>"red" }}
+        let(:error) {{}}
 
-        include_examples 'calendar color'
+        include_examples 'calendar color', :calendar_color, :error
       end
 
       context 'and day_in_month is saturday' do
-        let(:day_in_month) { DateTime.new(2020,9,05).strftime('%Y-%m-%d') }
+        let(:day_in_month) { "2020-9-05" }
         let(:calendar_color) {{ 5=>"red" }}
+        let(:error) {{}}
 
-        include_examples 'calendar color'
+        include_examples 'calendar color', :calendar_color, :error
       end
 
       context 'when day_in_month is normal day' do
-        let(:day_in_month) { DateTime.new(2020,9,02).strftime('%Y-%m-%d') }
+        let(:day_in_month) { "2020-9-02" }
         let(:calendar_color) {{ 2=>"red" }}
+        let(:error) {{}}
 
-        include_examples 'calendar color'
+        include_examples 'calendar color', :calendar_color, :error
       end
     end
 
     context 'when day_in_month is satuday' do
-      let(:day_in_month) { DateTime.new(2020,9,12).strftime('%Y-%m-%d') }
+      let(:day_in_month) {"2020-9-12" }
       let(:calendar_color) {{ 12=>"blue" }}
+      let(:error) {{}}
 
-      include_examples 'calendar color'
+      include_examples 'calendar color', :calendar_color, :error
     end
 
     context 'when day_in_month is sunday' do
-      let(:day_in_month) { DateTime.new(2020,9,20).strftime('%Y-%m-%d') }
+      let(:day_in_month) { "2020-9-20" }
       let(:calendar_color) {{ 20=>"red" }}
+      let(:error) {{}}
 
-      include_examples 'calendar color'
+      include_examples 'calendar color', :calendar_color, :error
     end
 
     context 'when day_in_month is normal day' do
-      let(:day_in_month) { DateTime.new(2020,9,10).strftime('%Y-%m-%d') }
+      let(:day_in_month) { "2020-9-10" }
       let(:calendar_color) {{ 10=>"black" }}
+      let(:error) {{}}
 
-      include_examples 'calendar color'
+      include_examples 'calendar color', :calendar_color, :error
     end
 
     context 'when day_in_month is incorrect format' do
-      let(:params) {{ day_in_month: "2020-12nnn-22" }}
+      let(:day_in_month) { "2020-12nnn-22" }
+      let(:calendar_color) {{}}
+      let(:error) {{:day_in_month=>:invalid}}
 
-      it { expect(assigns(:errors)).to eq ({:day_in_month=>:invalid}) }
+      include_examples 'calendar color', :calendar_color, :error
     end
 
     context 'when params is nil' do
       let(:params) { nil }
 
-      it { expect(assigns(:errors)).to eq ({}) }
+      it do
+        expect(assigns(:choose_day)).to eq ({})
+        expect(assigns(:errors)).to eq ({})
+      end
     end
   end
 end
