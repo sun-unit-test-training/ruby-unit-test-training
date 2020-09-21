822ea228bfd7912eca9083545b7b0289eaef0532
phongtv-0877 <truong.van.phong@sun-asterisk.com>
2020-09-21 21:28:00 +0700

    Exercise 3 Coding (#46)
    * Exercise 3 Coding
    
    * refactor code and edit test
    
    * fix comment
    
    * refactor

--- app/controllers/exercise3_controller.rb
+++ app/controllers/exercise3_controller.rb
@@ -1,5 +1,16 @@
 class Exercise3Controller < ApplicationController
   def index
-    @total_price
+    calulate = Exercise3::CalculateService.new(params_permit)
+    data = calulate.perform
+    @total_price = data[:total_price]
+    @discount_percent = data[:discount_percent]
+    @discount_price = data[:discount_price]
+    @errors = data[:errors]
+  end
+
+  private
+
+  def params_permit
+    params.permit(:white_shirt_amount, :tie_amount, other: {})
   end
 end
