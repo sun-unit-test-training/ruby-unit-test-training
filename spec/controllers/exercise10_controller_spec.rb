cfae8665849d7204eccb42f744dec5a532a99ea7
vovanquang12cntt <vanquang451@gmail.com>
2021-03-02 10:42:49 +0700

    Ex10 - Using mock and stub

--- spec/controllers/exercise10_controller_spec.rb
+++ spec/controllers/exercise10_controller_spec.rb
@@ -2,202 +2,41 @@ require "rails_helper"
 
 RSpec.describe Exercise10Controller do
   describe "POST checkout" do
-    let(:rank) { nil }
-    let(:give_coupon) { nil }
+    subject { post :checkout, params: params }
+
+    let(:rank) { "silver" }
+    let(:total_amount) { "3000" }
+    let(:discount_percent) { double :discount_percent }
+    let(:discount_amount) { double :discount_amount }
+    let(:total_amount_dou) { double :total_amount }
     let(:params) do
       {
         "rank":         rank,
         "total_amount": total_amount,
-        "give_coupon":  give_coupon
+        "give_coupon":  nil
       }
     end
-
-    before do
-      post :checkout, params: params
+    let(:response) do
+      OpenStruct.new(
+        success?:         true,
+        discount_percent: discount_percent,
+        discount_amount:  discount_amount,
+        total_amount:     total_amount_dou,
+        errors:           {}
+      )
     end
 
-    context "when checkout susccess" do
-      context "when apply discount corresponding with rank and total amount" do
-        context "when rank is `SILVER`" do
-          let(:rank) { "silver" }
+    it "return response data" do
+      service = instance_double(Exercise10::CalculateTotalAmountService)
+      allow(service).to receive(:perform).and_return(response)
+      allow(Exercise10::CalculateTotalAmountService).to receive(:new).with(rank, total_amount).and_return(service)
+      subject
 
-          context "when total amount is 3000円" do
-            let(:total_amount) { 3000 }
-
-            it "return amount discount 1%" do
-              expect(assigns(:response).total_amount).to eq 2970.0
-              expect(assigns(:response).discount_percent).to eq 1
-              expect(assigns(:response).discount_amount).to eq 30.0
-            end
-          end
-
-          context "when total amount is 5000円" do
-            let(:total_amount) { 5000 }
-
-            it "return amount discount 2%" do
-              expect(assigns(:response).total_amount).to eq 4900.0
-              expect(assigns(:response).discount_percent).to eq 2
-              expect(assigns(:response).discount_amount).to eq 100.0
-            end
-          end
-
-          context "when total amount is 10000円" do
-            let(:total_amount) { 10000 }
-
-            it "return amount discount 4%" do
-              expect(assigns(:response).total_amount).to eq 9600.0
-              expect(assigns(:response).discount_percent).to eq 4
-              expect(assigns(:response).discount_amount).to eq 400.0
-            end
-          end
-
-          include_examples "out ranger discount"
-        end
-
-        context "when rank is `GOLD`" do
-          let(:rank) { "gold" }
-
-          context "when total amount is 3000円" do
-            let(:total_amount) { 3000 }
-
-            it "return amount discount 3%" do
-              expect(assigns(:response).total_amount).to eq 2910.0
-              expect(assigns(:response).discount_percent).to eq 3
-              expect(assigns(:response).discount_amount).to eq 90.0
-            end
-          end
-
-          context "when total amount is 5000円" do
-            let(:total_amount) { 5000 }
-
-            it "return amount discount 5%" do
-              expect(assigns(:response).total_amount).to eq 4750.0
-              expect(assigns(:response).discount_percent).to eq 5
-              expect(assigns(:response).discount_amount).to eq 250.0
-            end
-          end
-
-          context "when total amount is 10000円" do
-            let(:total_amount) { 10000 }
-
-            it "return amount discount 10%" do
-              expect(assigns(:response).total_amount).to eq 9000.0
-              expect(assigns(:response).discount_percent).to eq 10
-              expect(assigns(:response).discount_amount).to eq 1000.0
-            end
-          end
-
-          include_examples "out ranger discount"
-        end
-
-        context "when rank is `PLATINUM`" do
-          let(:rank) { "platinum" }
-
-          context "when total amount is 3000円" do
-            let(:total_amount) { 3000 }
-
-            it "return amount discount 5%" do
-              expect(assigns(:response).total_amount).to eq 2850.0
-              expect(assigns(:response).discount_percent).to eq 5
-              expect(assigns(:response).discount_amount).to eq 150.0
-            end
-          end
-
-          context "when total amount is 5000円" do
-            let(:total_amount) { 5000 }
-
-            it "return amount discount 7%" do
-              expect(assigns(:response).total_amount).to eq 4650.0
-              expect(assigns(:response).discount_percent).to eq 7
-              expect(assigns(:response).discount_amount).to eq 350.0
-            end
-          end
-
-          context "when total amount is 10000円" do
-            let(:total_amount) { 10000 }
-
-            it "return amount discount 15%" do
-              expect(assigns(:response).total_amount).to eq 8500.0
-              expect(assigns(:response).discount_percent).to eq 15
-              expect(assigns(:response).discount_amount).to eq 1500.0
-            end
-          end
-
-          include_examples "out ranger discount"
-        end
-
-        context "when rank is another" do
-          let(:rank) { 0 }
-
-          it_behaves_like "do not discount", 3000
-          it_behaves_like "do not discount", 5000
-          it_behaves_like "do not discount", 10000
-        end
-      end
-
-      context "when coupon" do
-        shared_examples "don't have coupon" do |total_amount|
-          let(:total_amount) { total_amount }
-
-          it do
-            expect(assigns(:coupon)).to be nil
-          end
-        end
-
-        context "when have select give coupon" do
-          let(:give_coupon) { '1' }
-
-          context "when total amount is 5000円" do
-            let(:total_amount) { 5000 }
-
-            it do
-              expect(assigns(:coupon)).not_to be nil
-            end
-          end
-
-          context "when total amount is 10000円" do
-            let(:total_amount) { 10000 }
-
-            it do
-              expect(assigns(:coupon)).not_to be nil
-            end
-          end
-
-          it_behaves_like "don't have coupon", 4999
-          it_behaves_like "don't have coupon", 5001
-          it_behaves_like "don't have coupon", 9999
-          it_behaves_like "don't have coupon", 10001
-          it_behaves_like "don't have coupon", 3000
-        end
-
-        context "when don't have select give coupon" do
-          it_behaves_like "don't have coupon", 5000
-          it_behaves_like "don't have coupon", 10000
-          it_behaves_like "don't have coupon", 4999
-          it_behaves_like "don't have coupon", 5001
-          it_behaves_like "don't have coupon", 9999
-          it_behaves_like "don't have coupon", 10001
-          it_behaves_like "don't have coupon", 3000
-        end
-      end
-    end
-
-    context "when checkout failed" do
-      let(:rank) { 0 }
-
-      context "when total_amount is invalid" do
-        context "when total_amount has contains character" do
-          let(:total_amount) { "12asdas" }
-
-          it { expect(assigns(:errors)[:total_amount]).to eq :invalid }
-        end
-
-        context "when total_amount is a negative" do
-          let(:total_amount) { -1 }
-
-          it { expect(assigns(:errors)[:total_amount]).to eq :invalid }
-        end
-      end
+      expect(assigns(:response).total_amount).to eq total_amount_dou
+      expect(assigns(:response).discount_percent).to eq discount_percent
+      expect(assigns(:response).discount_amount).to eq discount_amount
+      expect(assigns(:coupon)).to eq nil
+      expect(assigns(:errors)).to eq response.errors
     end
   end
 
