8335ca33be52a7536b652a92871cb1404c10ee05
oLuongVanSang <luong.van.sang@sun-asterisk.com>
2021-03-04 15:11:04 +0700

    Mock stub exercise 7 (#61)
    * Mock stub exercise 7
    
    * Use allow instead of allow_any_instance_of
    
    * Replace random value with fixed value

--- spec/controllers/exercise_7_controller_spec.rb
+++ spec/controllers/exercise_7_controller_spec.rb
@@ -2,153 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Exercise7Controller, type: :controller do
   describe '#index' do
-    before { get :index, params: params }
+    let(:amount) { "10000" }
+    let(:fast_delivery) { "0" }
+    let(:premium) { "0" }
+    let(:delivery_fee) { 123 }
+    let(:result) { OpenStruct.new(delivery_fee: delivery_fee, errors: { foo: "bar"}) }
+    let(:service_instance) { instance_double(Exercise7::CalculateService, perform: result) }
 
-    context 'when params is valid' do
-      let(:params) do
-        {
-          premium: premium,
-          total_amount: total_amount,
-          fast_delivery: fast_delivery
-        }
-      end
+    it "should initialize service with received params" do
+      expect(Exercise7::CalculateService).to receive(:new).with(amount, fast_delivery, premium).and_call_original
 
-      context 'when user is not premium member' do
-        let(:premium) { '0' }
-
-        context 'when total_amount is less than 5000' do
-          let(:total_amount) { 4999 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 1000 }
-          end
-        end
-
-        context 'when total_amount is equal than 5000' do
-          let(:total_amount) { 5000 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 0 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-        end
-
-        context 'when total_amount is greater than 5000' do
-          let(:total_amount) { 5001 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 0 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-        end
-      end
-
-      context 'when user is premium member' do
-        let(:premium) { '1' }
-
-        context 'when total_amount is less than 5000' do
-          let(:total_amount) { 4999 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 0 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-        end
-
-        context 'when total_amount is equal than 5000' do
-          let(:total_amount) { 5000 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 0 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-        end
-
-        context 'when total_amount is greater than 5000' do
-          let(:total_amount) { 5001 }
-
-          context 'when do not have fast_delivery' do
-            let(:fast_delivery) { '0' }
-
-            it { expect(assigns(:delivery_fee)).to eq 0 }
-          end
-
-          context 'when have fast_delivery' do
-            let(:fast_delivery) { '1' }
-
-            it { expect(assigns(:delivery_fee)).to eq 500 }
-          end
-        end
-      end
+      get :index, params: {
+        total_amount: amount,
+        fast_delivery: fast_delivery,
+        premium: premium
+      }
     end
 
-    context 'when params is invalid' do
-      shared_examples 'invalid params' do |param|
-        it do
-          expect(assigns(:delivery_fee)).to eq 0
-          expect(assigns(:errors)[param]).to eq :invalid
-        end
-      end
+    it "should return delivery_fee and errors" do
+      allow(Exercise7::CalculateService).to receive(:new).and_return(service_instance)
+      get :index
 
-      context 'when total_amount is negative number' do
-        let(:params) do
-          {
-            premium: '1',
-            total_amount: -1,
-            fast_delivery: '1'
-          }
-        end
-
-        include_examples('invalid params', :total_amount)
-      end
-
-      context 'when total_amount is text' do
-        let(:params) do
-          {
-            premium: '1',
-            total_amount: 'abc',
-            fast_delivery: '1'
-          }
-        end
-
-        include_examples('invalid params', :total_amount)
-      end
+      expect(assigns(:delivery_fee)).to eq(delivery_fee)
+      expect(assigns(:errors)).to eq({ foo: "bar"})
+      expect(response).to render_template(:index)
     end
   end
 end
