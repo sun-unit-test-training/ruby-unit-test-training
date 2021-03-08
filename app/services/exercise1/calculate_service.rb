ce7b892fa67d7d1c6759d7ccc042277ccedb53bc
Vương Hưng <vuong.xuan.hung@sun-asterisk.com>
2021-03-08 09:38:22 +0700

    [Exercise 1] Refactor Unit Test to use stub/mock (#60)
    * [Exercise 1] Add docker-compose file
    
    * [Exercise 1] Refactor unit test to use stub/mock
    
    * [Exercise 1] Fix failed rubocop
    
    * [Exercise 1] Fix wrong argument in Service initializer
    
    * [Exercise 1] Fix comment - write subject block in one line and add more introduction about using docker

--- app/services/exercise1/calculate_service.rb
+++ app/services/exercise1/calculate_service.rb
@@ -1,6 +1,8 @@
 module Exercise1
   class CalculateService
-    def initialize(number_of_cup, time, have_voucher)
+    attr_accessor :number_of_cup, :time, :have_voucher, :errors, :total, :settings, :validations
+
+    def initialize(number_of_cup = nil, time = nil, have_voucher = nil)
       @validations = Settings.validations
       @settings = Settings.exercise_1
       @number_of_cup = number_of_cup
@@ -11,11 +13,11 @@ module Exercise1
     end
 
     def perform
-      return response(true) if @number_of_cup.nil? || @time.nil?
+      return response(true) if number_of_cup.nil? || time.nil?
 
-      @number_of_cup = validate_number_of_cup(@number_of_cup)
-      @time = validate_time(@time)
-      @total = @number_of_cup.positive? ? (@number_of_cup - 1) * price_at_time + price_of_first_cup : 0
+      @number_of_cup = validate_number_of_cup
+      @time = validate_time
+      @total = total_price
 
       response(true)
     rescue ArgumentError
@@ -24,28 +26,42 @@ module Exercise1
 
     private
 
-    def validate_number_of_cup(number)
-      return number.to_i if number.to_s.match?(@validations.number)
+    def validate_number_of_cup
+      return number_of_cup.to_i if number_of_cup.to_s.match?(validations.number)
 
-      @errors.merge!(number_of_cup: :invalid)
+      errors.merge!(number_of_cup: :invalid)
       raise ArgumentError
     end
 
-    def validate_time(time)
-      return Time.zone.parse(time) if time.to_s.match?(@validations.time)
+    def validate_time
+      return Time.zone.parse(time) if time.to_s.match?(validations.time)
 
-      @errors.merge!(time: :invalid)
+      errors.merge!(time: :invalid)
       raise ArgumentError
+    end
+
+    def discount_time?
+      time.between?(*discount_time)
+    end
+
+    def voucher?
+      have_voucher == '1'
     end
 
     def price_at_time
       @price_at_time ||=
-        @time.between?(*discount_time) ? @settings.price_within_discount_time : @settings.price_per_cup
+        discount_time? ? settings.price_within_discount_time : settings.price_per_cup
     end
 
     def price_of_first_cup
       @price_of_first_cup ||=
-        @have_voucher == '1' ? @settings.price_with_voucher : price_at_time
+        voucher? ? settings.price_with_voucher : price_at_time
+    end
+
+    def total_price
+      return 0 unless number_of_cup.positive?
+
+      (number_of_cup - 1) * price_at_time + price_of_first_cup
     end
 
     def discount_time
@@ -56,7 +72,7 @@ module Exercise1
     end
 
     def response(result)
-      OpenStruct.new(success?: result, total: @total, errors: @errors)
+      OpenStruct.new(success?: result, total: total, errors: errors)
     end
   end
 end
